11ddeba81efe7d812cd15a70ee834f6ea6a9fe05
Jonathan Wexler <jwexman@gmail.com>
2016-07-13 16:54:41 -0400

    more data

--- generator_example/app.js
+++ generator_example/app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.locals.data = require('./data.json')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- generator_example/routes/index.js
+++ generator_example/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// One park
+router.get('/park/:name', function(req, res, next) {
+  var approved_parks = [];
+  data.national_parks.forEach(function (park) {
+    if (park.name.toLowerCase() === req.params.name.toLowerCase()) {
+      approved_parks.push(park);
+    };
+  })
+  res.render('parks', { title: 'Park Search', parks: approved_parks });
+});
+
+// All parks
+router.get('/parks', function(req, res, next) {
+  res.render('parks', { title: 'Park Search', parks: data.national_parks });
+});
+
+// Post to parks
+router.post('/park', function(req, res, next) {
+  if(typeof(req.body.name) !== 'undefined' && req.body.name !== '') data.national_parks.push({"name": req.body.name});
+  res.redirect('/parks');
 });
 
 module.exports = router;
